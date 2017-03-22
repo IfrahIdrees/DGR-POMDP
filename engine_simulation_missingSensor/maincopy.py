7f5c2ee4de614e663c158225b6218f2b59911fd5
danwangkoala <danwangkoala@gmail.com>
2017-03-22 10:50:02 -0400

    more experiment result

--- engine_simulation_missingSensor/maincopy.py
+++ engine_simulation_missingSensor/maincopy.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     nothing_happen = 0.01
     
     ##the otherHappen triggering threshhold
-    other_happen = 0.80
+    other_happen = 1.2
     
     file_num = 4
     #sensor_reliability = [None, 0.9, 0.95, 0.8]
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     #files = [1, 2, 3]
     #sensor_reliability = [-1, 0.5, -2]
     #for file_num in range(4, 8):
-    for file_num in range(11, 13):
+    for file_num in range(5, 7):
         for x in sensor_reliability:
             ##the output file name
             output_file_name = "Case" + str(file_num) + "_" + str(x) + ".txt"
