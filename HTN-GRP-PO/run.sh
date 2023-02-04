2c84c6a37a4ad7f2c392f31f3a92cfd77c1ac460
ifrah <ifrah_idrees@brown.edu>
2023-02-04 03:33:00 -0500

    Feat: added run.sh

---
+++ HTN-GRP-PO/run.sh
@@ -0,0 +1,5 @@
+# python main.py --agent_type fixed_always_ask --max_depth 17 --num_sims 5 --d 0.95 --e 1 --wp -5 --qr 5 --qp -5 --domain kitchen 
+python main.py --agent_type pomdp --max_depth 19 --num_sims 6 --d 0.95 --e 1 --wp -5 --qr 5 --qp -5 --domain block
+# python main.py --agent_type fixed_always_ask --max_depth 17 --num_sims 5 --d 0.95 --e 1 --wp -5 --qr 5 --qp -5 --domain block
+# python main.py --agent_type htn --max_depth 17 --num_sims 5 --d 0.95 --e 1 --wp -5 --qr 5 --qp -5 --domain block
+# python main.py --agent_type fixed_always_ask --max_depth 17 --num_sims 5 --d 0.95 --e 1 --wp -5 --qr 5 --qp -5 --domain block
